f466e37b23e3128fb637ee52f9c23f726332fecd
david921518 <david921518@qq.com>
2025-02-07 05:21:16 +0800

    Update build_ext4fsd.cmd

--- Setup/build_ext4fsd.cmd
+++ Setup/build_ext4fsd.cmd
@@ -13,10 +13,10 @@ TIMEOUT /T 3 /NOBREAK
 signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256 ..\Ext4Fsd\Release\x64\Ext4Fsd.sys
 TIMEOUT /T 3 /NOBREAK
 
-signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256 ..\Ext4Fsd\Ext4Fsd.inf
+signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256 ..\Ext4Fsd\Ext2Fsd.inf
 TIMEOUT /T 3 /NOBREAK
 
 makensis ext4fsd.nsi
 TIMEOUT /T 3 /NOBREAK
 
-signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256  Ext4Fsd-setup.exe
+signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256  Ext2Fsd-setup.exe
