f78fc4fe3ff6b63e534ff50d634110cc536796de
david921518 <david921518@qq.com>
2025-02-07 01:15:47 +0800

    Create build_ext4fsd.cmd

---
+++ Setup/build_ext4fsd.cmd
@@ -0,0 +1,22 @@
+signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256 ..\Ext2Mgr\Release\x86\Ext2Mgr.exe
+TIMEOUT /T 3 /NOBREAK
+signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256 ..\Ext2Mgr\Release\x64\Ext2Mgr.exe
+TIMEOUT /T 3 /NOBREAK
+
+signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256 ..\Ext2Srv\Release\x86\Ext2Srv.exe
+TIMEOUT /T 3 /NOBREAK
+signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256 ..\Ext2Srv\Release\x64\Ext2Srv.exe
+TIMEOUT /T 3 /NOBREAK
+
+signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256 ..\Ext4Fsd\Release\x86\Ext4Fsd.sys
+TIMEOUT /T 3 /NOBREAK
+signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256 ..\Ext4Fsd\Release\x64\Ext4Fsd.sys
+TIMEOUT /T 3 /NOBREAK
+
+signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256 ..\Ext4Fsd\Ext4Fsd.inf
+TIMEOUT /T 3 /NOBREAK
+
+makensis ext4fsd.nsi
+TIMEOUT /T 3 /NOBREAK
+
+signtool sign /a /as /v /fd sha256 /sha1 9a7e6042a8a5f2721684ec8c3cb864835c0d10c5 /tr http://time.certum.pl /td sha256  Ext4Fsd-setup.exe
